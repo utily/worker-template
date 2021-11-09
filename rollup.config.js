2f57a7d7072677c34a761b71d6d42cfb072efb0f
Simon Mika <simon@mika.se>
2021-11-09 14:29:32 +0100

    Improved configuration.

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
-import { terser } from 'rollup-plugin-terser'
+import { terser } from "rollup-plugin-terser"
 // plugin-node-resolve and plugin-commonjs are required for a rollup bundled project
 // to resolve dependencies from node_modules. See the documentation for these plugins
 // for more details.
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "@rollup/plugin-typescript"
 
 export default {
-  input: 'index.ts',
+  input: "index.ts",
   output: {
-    exports: 'named',
-    format: 'es',
-    file: 'dist/index.mjs',
+    exports: "named",
+    format: "es",
+    file: "dist/index.mjs",
     sourcemap: true,
   },
   plugins: [commonjs(), nodeResolve({ browser: true }), terser(), typescript()],
