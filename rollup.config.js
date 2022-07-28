e2816bb8f07d88bc301f2016b09da52dd2269862
Elias Eriksson <eliaseriksson95@gmail.com>
2022-07-28 17:06:36 +0200

    now uses tasks instead of exessive bash

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ export default {
   output: {
     exports: "named",
     format: "es",
-    file: "dist/index.mjs",
+    file: "dist/_worker.js",
     sourcemap: true,
 		sourcemapPathTransform: relativeSourcePath => path.resolve(__dirname, relativeSourcePath.replace(/^(..\/)+/, "")),
   },
