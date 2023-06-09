352e2377eb17e049cc799f58cf9b4f06df00fe28
Simon Mika <simon@mika.se>
2023-06-09 16:01:31 +0200

    Broken.

--- Context/Users/User.ts
+++ Context/Users/User.ts
@@ -1,14 +1,19 @@
+import { cryptly } from "cryptly"
 import * as isoly from "isoly"
 import { isly } from "isly"
 import { model } from "../../model"
 
-export type User = model.User & model.User.Creatable
+export type User = model.User & { password?: cryptly.Password.Hash }
 
 export namespace User {
-	export function fromModel(user: model.User.Creatable, admin: string): User {
-		return { ...user, created: [isoly.DateTime.now(), admin] }
+	export type V1 = model.User & model.User.Creatable
+	export function fromModel(user: model.User.Creatable & Pick<User, "password">, admin: string): User {
+		return {
+			...user,
+			created: [isoly.DateTime.now(), admin],
+		}
 	}
-	export function toModel(user: User | undefined): model.User | undefined {
+	export function toModel(user: User | User.V1 | undefined): model.User | undefined {
 		return model.User.type.get(user)
 	}
 	export const type = isly.named(
--- Context/Users/index.ts
+++ Context/Users/index.ts
@@ -1,3 +1,4 @@
+import { cryptly } from "cryptly"
 import * as gracely from "gracely"
 import { http } from "cloudly-http"
 import * as storage from "cloudly-storage"
@@ -5,9 +6,15 @@ import { model } from "../../model"
 import { User } from "./User"
 
 export class Users {
-	private constructor(private readonly store: storage.KeyValueStore<User>) {}
+	private constructor(private readonly store: storage.KeyValueStore<User>, private signer: any) {}
+	private async hash(password: string | undefined, salt?: string): Promise<cryptly.Password | undefined> {
+		return password == undefined ? undefined : cryptly.Password.hash(this.signer, password, salt)
+	}
+	private async verify(password: string, hash: cryptly.Password.Hash | undefined): Promise<boolean> {
+		return hash != undefined && (await cryptly.Password.verify(this.signer, hash, password))
+	}
 	async add(user: model.User.Creatable, admin: string): Promise<model.User | undefined> {
-		const result: User = User.fromModel(user, admin)
+		const result: User = User.fromModel({ ...user, password: await this.hash(user.password) }, admin)
 		await this.store.set(user.email, result)
 		return User.toModel(result)
 	}
@@ -25,12 +32,13 @@ export class Users {
 	async list(options?: storage.KeyValueStore.ListOptions): Promise<storage.Continuable<model.User | undefined>> {
 		return (await this.store.list(options)).map(v => User.toModel(v.value as User))
 	}
-	static create(store: storage.KeyValueStore<string, string>): Users | gracely.Error {
+	static create(store: storage.KeyValueStore<string, string>, pepper: string): Users | gracely.Error {
 		return new Users(
 			storage.KeyValueStore.partition(
 				storage.KeyValueStore.Json.create(storage.KeyValueStore.OnlyMeta.create<string>(store)),
 				"user|"
-			)
+			),
+			cryptly.Signer.create("HMAC", "SHA-512", pepper)
 		)
 	}
 }
